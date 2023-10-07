c4728496360436dc1c7c1de458c84596f97621d1
Belarif <b.ocine@live.fr>
2023-10-07 13:32:49 +0200

    ss1: creating and integrating vertical navigation component

--- front-end/src/pages/Profil.js
+++ front-end/src/pages/Profil.js
@@ -1,8 +1,15 @@
 import "../sass/main.scss";
 import HorizontalNavigation from "../components/HorizontalNavigation";
+import VerticalNavigation from "../components/VerticalNavigation";
+import React from "react";
 
 const Profil = () => {
-  return <HorizontalNavigation />;
+  return (
+    <React.Fragment>
+      <HorizontalNavigation />
+      <VerticalNavigation />
+    </React.Fragment>
+  );
 };
 
 export default Profil;
