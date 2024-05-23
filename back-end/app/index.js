7f45756c4298fb24f2b56cc36b9abf51bf53dfcb
Belarif <b.ocine@live.fr>
2024-05-23 12:42:40 +0200

    master: editing index file

--- back-end/app/index.js
+++ back-end/app/index.js
@@ -10,3 +10,13 @@ const port = 3000;
 app.use(router);
 
 app.listen(port, () => console.log(`Magic happens on port ${port}`));
+
+// verify server route
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// catch all route
+app.use((req, res, next) => {
+  res.status(404).send("Sorry, we could not find that!");
+});
